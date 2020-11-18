9ce9cd976581625066680f845cfed3e634df5741
Preston Sharpe <pjsharpe07@gmail.com>
2020-11-17 17:30:22 -0700

    ckan-worker initial commit - untested

---
+++ ckan-worker/docker-entrypoint.d/start_ckan.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Run the prerun script to init CKAN and create the default admin user
+sudo -u ckan -EH python prerun.py
+
+# Run any startup scripts provided by images extending this one
+if [[ -d "/docker-entrypoint.d" ]]
+then
+    for f in /docker-entrypoint.d/*; do
+        case "$f" in
+            *.sh)     echo "$0: Running init file $f"; . "$f" ;;
+            *.py)     echo "$0: Running init file $f"; python "$f"; echo ;;
+            *)        echo "$0: Ignoring $f (not an sh or py file)" ;;
+        esac
+        echo
+    done
+fi
+
+# Start supervisor
+if [ $? -eq 0 ]
+    supervisord --configuration /etc/supervisord.conf &
+else
+  echo "[prerun] failed...not starting CKAN."
+fi
