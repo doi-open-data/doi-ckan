87007a3a47c63f2c7222206b910d7e10004a0358
jbrown-xentity <jbrown@xentity.com>
2020-12-01 11:42:47 -0700

    Wait until web ready, remove sleep

--- ckan/start_ckan_worker.sh
+++ ckan/start_ckan_worker.sh
@@ -8,9 +8,17 @@ paster --plugin=ckan config-tool $CKAN_INI -s DEFAULT "debug = true"
 echo "Loading the following plugins: $CKAN__PLUGINS"
 paster --plugin=ckan config-tool $CKAN_INI "ckan.plugins = $CKAN__PLUGINS"
 
-# # Run the prerun script to init CKAN and create the default admin user
-# sudo -u ckan -EH python prerun.py
-sleep 100
+# Wait for the ckan-web service to be serving traffic before starting
+function wait_for () {
+  local host=$1
+  local port=$2
+
+  while ! nc -z -w 5 "$host" "$port"; do
+    sleep 1
+  done
+}
+
+wait_for ckan-web 5000
 
 chown root:root /etc/crontabs/root && /usr/sbin/crond -f & 
 supervisord --configuration /etc/supervisord.conf
