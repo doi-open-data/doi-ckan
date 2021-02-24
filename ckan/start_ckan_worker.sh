3eed93d60a680c6b6b91bac85b792199112b4cbd
Preston Sharpe <psharpe@xentity.com>
2021-02-23 21:08:40 -0700

    minor changes for production ready

--- ckan/start_ckan_worker.sh
+++ ckan/start_ckan_worker.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-# Set debug to true
-echo "Enabling debug mode"
-paster --plugin=ckan config-tool $CKAN_INI -s DEFAULT "debug = true"
-
 # Update the plugins setting in the ini file with the values defined in the env var
 echo "Loading the following plugins: $CKAN__PLUGINS"
 paster --plugin=ckan config-tool $CKAN_INI \
@@ -20,7 +16,7 @@ function wait_for () {
   done
 }
 
-wait_for ckan-web 5000
+wait_for $1 $2
 
 chown root:root /etc/crontabs/root && /usr/sbin/crond -f & 
 supervisord --configuration /etc/supervisord.conf
